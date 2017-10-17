a879db205b01f4a8466fb59267ffc192609d1cbd
maramalquraan <quraanmaram@gmail.com>
2017-10-17 19:53:26 +0300

    find the location and mark it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Geolocation } from '@ionic-native/geolocation';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 import { SignUpPage } from "../pages/sign-up/sign-up";
-import {MainPage} from "../pages/main/main";
-import {MapComponent} from "../components/map/map";
-
+import {MainPage} from "../pages/main/main"
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     SignUpPage,
-    MainPage,
-    MapComponent
+    MainPage
   ],
   imports: [
     BrowserModule,
@@ -33,6 +30,7 @@ import {MapComponent} from "../components/map/map";
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Geolocation } from '@ionic-native/geolocation';
 /**
  * Generated class for the MainPage page.
  *
@@ -14,29 +14,69 @@ declare var google: any;
   selector: 'page-main',
   templateUrl: 'main.html',
 })
+
 export class MainPage {
   @ViewChild('map') mapElement:ElementRef;
   map: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
   }
 
   ionViewDidLoad() {
     this.initMap();
+  
     console.log('ionViewDidLoad MainPage');
   }
   
   loadSideMenu(){
-    console.log("clicked")
+    console.log("clicked");
   }
 
   initMap(){
-    let location= new google.maps.LatLng(-34.9290, 138.6010);
-    let mapOptions = {
-      center: location,
-      zoom:15,
-      // mapTypeId: google.maps.mapTypeId.ROADMAP
-      // mapTypeId: google.maps.MapType.G_NORMAL_MAP
-    };
-    this.map=new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-  }
-}
+  this.geolocation.getCurrentPosition().then((position) => {
+    
+         let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    
+         let mapOptions = {
+           center: location,
+           zoom: 15,
+           mapTypeId: google.maps.MapTypeId.ROADMAP
+          // mapTypeId: google.maps.MapType.G_NORMAL_MAP
+
+         }
+    
+         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    
+       });
+    
+     }
+
+     addMarker(){
+      
+       let marker = new google.maps.Marker({
+         map: this.map,
+         animation: google.maps.Animation.DROP,
+         position: this.map.getCenter()
+       });
+      
+       let content = "<h4>Information..</h4>";         
+      
+       this.addInfoWindow(marker, content);
+      
+     }
+    
+     addInfoWindow(marker, content){
+      
+       let infoWindow = new google.maps.InfoWindow({
+         content: content
+       });
+      
+       google.maps.event.addListener(marker, 'click', () => {
+         infoWindow.open(this.map, marker);
+       });
+      
+     }
+    
+   }
+
+ 
+
