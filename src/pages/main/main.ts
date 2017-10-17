b0b93e86a78f62b5dad0bd6a2cd6824dc4ed0436
maramalquraan <quraanmaram@gmail.com>
2017-10-17 15:39:41 +0300

    show the map on the website

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {MapComponent} from "../../components/map/map";
-
 
 /**
  * Generated class for the MainPage page.
@@ -9,20 +7,36 @@ import {MapComponent} from "../../components/map/map";
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+declare var google: any;
 
 @IonicPage()
 @Component({
   selector: 'page-main',
   templateUrl: 'main.html',
-  directives: [MapComponent],
 })
 export class MainPage {
-
+  @ViewChild('map') mapElement:ElementRef;
+  map: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+    this.initMap();
     console.log('ionViewDidLoad MainPage');
   }
+  
+  loadSideMenu(){
+    console.log("clicked")
+  }
 
+  initMap(){
+    let location= new google.maps.LatLng(-34.9290, 138.6010);
+    let mapOptions = {
+      center: location,
+      zoom:15,
+      // mapTypeId: google.maps.mapTypeId.ROADMAP
+      // mapTypeId: google.maps.MapType.G_NORMAL_MAP
+    };
+    this.map=new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 }
