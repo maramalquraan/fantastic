1c5470964886a462f316d1f5c4d35ad2163417ef
hibaAkroush <hiba.akr@gmail.com>
2017-10-19 10:42:40 +0300

    changed cridentials to the public one and fixed the redirection from sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,12 +22,12 @@ import {MainPage} from "../pages/main/main";
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyC6hAjtXhxf1VZwsIJkNyJRj7fb7NK_Qas",
-      authDomain: "fantastic-95f95.firebaseapp.com",
-      databaseURL: "https://fantastic-95f95.firebaseio.com",
-      projectId: "fantastic-95f95",
-      storageBucket: "fantastic-95f95.appspot.com",
-      messagingSenderId: "420995755079"
+      apiKey: "AIzaSyAUipRdjwgFm76lPfFCVWH84OWKSY5S32I",
+      authDomain: "fantastic-13633.firebaseapp.com",
+      databaseURL: "https://fantastic-13633.firebaseio.com",
+      projectId: "fantastic-13633",
+      storageBucket: "fantastic-13633.appspot.com",
+      messagingSenderId: "1014964753035"
     }),
     AngularFireAuthModule
   ],
